8a2692a2719d25105ce507cfdb6ce21cb1a5338b
gokhandogan7 <gdogan3746@gmail.com>
2020-11-24 23:26:40 +0100

    env file and firebase function has created

--- project-app/src/firebase/firebase.utils.js
+++ project-app/src/firebase/firebase.utils.js
@@ -1,4 +1,7 @@
-/* const firebaseConfig = {
+import firebase from 'firebase/app';
+
+
+const devConfig = {
   apiKey: "AIzaSyCxH7UCFPmwHpN_hcD_Hx9OAmRNkwC2NWE",
   authDomain: "reactshare2288.firebaseapp.com",
   databaseURL: "https://reactshare2288.firebaseio.com",
@@ -6,4 +9,22 @@
   storageBucket: "reactshare2288.appspot.com",
   messagingSenderId: "86672859511",
   appId: "1:86672859511:web:7794ccd8e5d01e5f51c172"
-}; */
+};
+
+//auth
+//firestore
+
+const prodConfig ={}
+
+const config = process.env.NODE_ENV === 'development' ? devConfig : 'prodConfig'
+
+
+class Firebase {
+    constructor() {
+        firebase.initializeApp(config);
+    }
+}
+
+export default new Firebase()
+
+//firebase.initializeApp(devConfig)
