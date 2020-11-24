47f02c49d14a25ae1a4c2766e8340d3c1f37803c
gokhandogan7 <gdogan3746@gmail.com>
2020-11-24 21:21:44 +0100

    signup and firebase are created

---
+++ project-app/src/firebase/firebase.utils.js
@@ -0,0 +1,9 @@
+/* const firebaseConfig = {
+  apiKey: "AIzaSyCxH7UCFPmwHpN_hcD_Hx9OAmRNkwC2NWE",
+  authDomain: "reactshare2288.firebaseapp.com",
+  databaseURL: "https://reactshare2288.firebaseio.com",
+  projectId: "reactshare2288",
+  storageBucket: "reactshare2288.appspot.com",
+  messagingSenderId: "86672859511",
+  appId: "1:86672859511:web:7794ccd8e5d01e5f51c172"
+}; */
